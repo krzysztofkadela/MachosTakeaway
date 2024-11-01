5317f336afddefaa150fe6051c07b6ca6cda722a
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-11-01 22:15:42 +0000

    Fix: security for keys.

--- takeaway/settings.py
+++ takeaway/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jjnwv1t4qe3(w#ito#mdic$9$k&rvmdj%u$xmlq=9!-yl!qhl&'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1',]
 
 
 # Application definition
