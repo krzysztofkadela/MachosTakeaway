d032ac0cb87dd167b7dfc37e159a872b9b4cf998
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-11-02 18:19:45 +0000

    Add main page html file.

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
+
 def index(request):
-    return HttpResponse("Helo world!")
+    return render(request, 'mainpage/index.html') # Render main page index.html.
