32608d0c79fbd6ed7ca8070a66ec75f44f39c88b
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-11-04 20:44:43 +0000

    Update models About with created_on and updated on.

--- about/models.py
+++ about/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 
 class About(models.Model):
     heading = models.CharField(max_length=200)
     content = models.TextField()
     image = models.ImageField(upload_to='about_images/')
-
+    created_on = models.DateTimeField(default=timezone.now)  # Automatically set on creation
+    updated_on = models.DateTimeField(auto_now=True)  # Automatically set on update
     def __str__(self):
         return self.heading
