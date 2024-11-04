d7e7c98dbc17af964b7ba60e19fe93e479fec4d0
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-11-04 19:07:37 +0000

    Preaper for heroku deploy.

--- takeaway/settings.py
+++ takeaway/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1','http://8000-krzysztofka-machostakea-35f67se8832.ws-eu116.gitpod.io/']
 
