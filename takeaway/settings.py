ad5ddb12839eb8cceca26834e505a6a728066a09
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-11-04 21:19:08 +0000

    Fix: image file for about page.

--- takeaway/settings.py
+++ takeaway/settings.py
@@ -28,9 +28,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1','http://8000-krzysztofka-machostakea-35f67se8832.ws-eu116.gitpod.io/']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1','http://8000-krzysztofka-machostakea-35f67se8832.ws-eu116.gitpod.io/,']
 
 
 # Application definition
