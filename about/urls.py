ae8195ce2cf5cf70c7f5790d5e48474f60aad9bd
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-11-04 20:07:29 +0000

    Add model for about page.

--- about/urls.py
+++ about/urls.py
@@ -1,8 +1,13 @@
 # about/urls.py
 from django.urls import path
-from .views import about, redirect_about_html
+from .views import about_view, redirect_about_html
+
+#urlpatterns = [
+#    path('', about, name='about'), #respond to /about/ 
+#   path('about.html', redirect_about_html, name='redirect_about_html'), # Redirect from /about.html to /about/ 
+#]
 
 urlpatterns = [
-    path('', about, name='about'), #respond to /about/ 
-    path('about.html', redirect_about_html, name='redirect_about_html'), # Redirect from /about.html to /about/ 
+    path('about/', about_view, name='about'),  # This should match the redirect
+    path('about/redirect/', redirect_about_html, name='redirect_about'),
 ]
