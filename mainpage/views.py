2be924655559ebb3d098a97fadcd79bcdaefff79
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-11-05 21:18:00 +0000

    Fix: Comments nots displaying: update views.

--- mainpage/views.py
+++ mainpage/views.py
@@ -3,10 +3,16 @@ from .models import CustomerComment
 
 # Create your views here.
 
+#def index(request):
+#   return render(request, 'mainpage/index.html') # Render main page index.html.
+
+
+#def homepage(request):
+#   comments = CustomerComment.objects.filter(is_approved=True).order_by('-comment_date')[:10]  # Fetch last 10 approved comments
+#   return render(request, 'index.html', {'comments': comments})
+
 def index(request):
-    return render(request, 'mainpage/index.html') # Render main page index.html.
-
-
-def homepage(request):
-    comments = CustomerComment.objects.filter(is_approved=True).order_by('-comment_date')[:10]  # Fetch last 10 approved comments
-    return render(request, 'your_template_name.html', {'comments': comments})
+    # Fetch the last 10 approved comments that are updated most recently
+    comments = CustomerComment.objects.filter(is_approved=True).order_by('-updated_on')[:10]
+    
+    return render(request, 'mainpage/index.html', {'comments': comments})  # Render the homepage template with comments
