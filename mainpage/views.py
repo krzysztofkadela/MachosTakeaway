418b0368efa2a4bcfb34111a3811b76231de3ebf
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-11-05 20:45:57 +0000

    Add models to store customer comments.

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-
+from .models import CustomerComment
 
 # Create your views here.
 
 def index(request):
     return render(request, 'mainpage/index.html') # Render main page index.html.
+
+
+def homepage(request):
+    comments = CustomerComment.objects.filter(is_approved=True).order_by('-comment_date')[:10]  # Fetch last 10 approved comments
+    return render(request, 'your_template_name.html', {'comments': comments})
