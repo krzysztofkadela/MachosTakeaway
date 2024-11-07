bbb2e8b9529ae8cd24eb0d8ff4ee478d89c40d9e
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-11-07 21:03:18 +0000

    Index html correction.

--- takeaway/settings.py
+++ takeaway/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1','8000-krzysztofka-machostakea-35f67se8832.ws-eu116.gitpod.io/,']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1','8000-krzysztofka-machostakea-35f67se8832.ws-us116.gitpod.io/']
 
 
 # Application definition
