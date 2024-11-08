7a9131af1bf37253dde61c2d1eecc0d3b0f7b0e4
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-11-08 20:22:45 +0000

    Authentication and Authorization.

--- takeaway/urls.py
+++ takeaway/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('menu/', include('menu.urls')), # Include the menu app URL's
     path('', include('mainpage.urls')),  # Include the URLs from mainpage app
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),  # Include allauth URLs for account management
 ]
 
 if settings.DEBUG:
