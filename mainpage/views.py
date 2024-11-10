1909d2b9c9619d2035e21de44782c9a40b3b4274
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-11-10 15:19:08 +0000

    Fix: Basic register and lgin forms working on main page.

--- mainpage/views.py
+++ mainpage/views.py
@@ -39,7 +39,7 @@ def custom_logout(request):
     logout(request)
     return redirect('index')  # Redirect after logging out
 
-def search_view(request):
-    query = request.GET.get('q')  # Assuming the search form uses a parameter 'q'
-    results = []  # Populate this with actual search results based on your logic
-    return render(request, 'search_results.html', {'results': results})  # Point to your template
+#def search_view(request):
+#   query = request.GET.get('q')  # Assuming the search form uses a parameter 'q'
+#   results = []  # Populate this with actual search results based on your logic
+#  return render(request, 'search_results.html', {'results': results})  # Point to your template
