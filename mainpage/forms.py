2126a66db3724ff5cd5b21e40d4facc9423505f7
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-11-11 21:13:32 +0000

    Comment otpion added for loged users.

--- mainpage/forms.py
+++ mainpage/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
+from .models import CustomerComment
 
 class CustomLoginForm(AuthenticationForm):
 
@@ -10,3 +11,8 @@ class CustomLoginForm(AuthenticationForm):
         self.helper = FormHelper(self)
         self.helper.form_method = 'post'
         self.helper.add_input(Submit('submit', 'Login', css_class='btn btn-primary'))
+
+class CustomerCommentForm(forms.ModelForm):
+    class Meta:
+        model = CustomerComment
+        fields = ['comment']  # Include comments from comment moddel
--- mainpage/views.py
+++ mainpage/views.py
@@ -1,23 +1,46 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
+from .forms import CustomerCommentForm
 from .models import CustomerComment
 from .forms import CustomLoginForm
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
 #def index(request):
 #   return render(request, 'mainpage/index.html') # Render main page index.html.
+def index(request):
+    """Fetch the last 10 approved comments that are updated most recently."""
+    comments = CustomerComment.objects.filter(is_approved=True).order_by('-updated_on')[:10]
+    return render(request, 'mainpage/index.html', {'comments': comments})  # Render the homepage template with comments
 
+@login_required  # Ensure only logged-in users can access this view
+def add_comment(request):
+    """View to handle adding a new comment."""
+    if request.method == 'POST':
+        form = CustomerCommentForm(request.POST)
+        if form.is_valid():
+            # Create a new comment instance but don't save it to the database yet
+            comment = form.save(commit=False)
+            comment.user = request.user  # Set the user to the currently logged-in user
+            comment.save()  # Save the comment in the database
+            return redirect('index')  # Redirect to the homepage or wherever appropriate
+    else:
+        form = CustomerCommentForm()  # Create an empty form instance
+
+    return render(request, 'add_comment.html', {'comment_form': form})  # Render a page for adding comments (can be a separate template)
 
 #def homepage(request):
 #   comments = CustomerComment.objects.filter(is_approved=True).order_by('-comment_date')[:10]  # Fetch last 10 approved comments
 #   return render(request, 'index.html', {'comments': comments})
 
-def index(request):
+
+#def index(request):
     # Fetch the last 10 approved comments that are updated most recently
-    comments = CustomerComment.objects.filter(is_approved=True).order_by('-updated_on')[:10]
+ #  comments = CustomerComment.objects.filter(is_approved=True).order_by('-updated_on')[:10]
     
-    return render(request, 'mainpage/index.html', {'comments': comments})  # Render the homepage template with comments
+  #  return render(request, 'mainpage/index.html', {'comments': comments})  # Render the homepage template with comments
 
 def custom_login(request):
     """Handle login with CustomLoginForm."""
