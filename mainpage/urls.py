c50629d1bdbed284a489dccc3bbb184a55ff39f1
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-11-16 17:12:06 +0000

    Edit and Delete functionality added to comment section.

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import index, add_comment # Import your index view
-from .views import custom_login, custom_logout, user_comments #search_view
+from .views import index, add_comment, edit_comment, delete_comment  # Importing views
+from .views import custom_login, custom_logout, user_comments #importing views line 2
 
 urlpatterns = [
     path('', index, name='index'),  # Map the root URL to the index view
     path('add_comment/', add_comment, name='add_comment'),
+    path('comments/edit/<int:comment_id>/', edit_comment, name='edit_comment'),
+    path('comments/delete/<int:comment_id>/', delete_comment, name='delete_comment'),
     path('user_comments/', user_comments, name='user_comments'),
-    path('login/', custom_login, name='login'),  # Ensure this path exists
+    path('login/', custom_login, name='login'),  # login path
     path('logout/', custom_logout, name='logout'),
 ]
