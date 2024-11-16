778025426d53e7717452e58bbfbb839a18e96443
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-11-16 19:01:34 +0000

    Model for menu items.

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import MenuItem
 
 # Register your models here.
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'price')
+    search_fields = ('title', 'description')
+    list_filter = ('category',)
+
+admin.site.register(MenuItem, MenuItemAdmin)
