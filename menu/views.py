dbdaa6edebee4f97baaf590dd69c94e5bb07192a
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-11-20 20:26:03 +0000

    feat:Correctly retrieved MenuItems from the database in the menu view

--- menu/views.py
+++ menu/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
+from .models import MenuItem  # Make sure to import your MenuItem model
 
 # Create your views here.
 
 def menu(request):
-    return render(request, 'menu/menu.html')
+    menu_items = MenuItem.objects.all()  # Retrieve all MenuItem objects
+    return render(request, 'menu/menu.html', {'menu_items': menu_items})  # Pass them to the template
 
 def redirect_menu_html(request):
     return redirect('menu')  # Redirect to the 'menu' named URL
