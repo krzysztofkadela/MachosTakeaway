a254823f84f1ffb847a6aea17bb363f40dc87b5c
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-11-23 22:29:49 +0000

    Fix: Menu item display update.

--- menu/models.py
+++ menu/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify # imports slug field 
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
@@ -15,7 +16,13 @@ class MenuItem(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
-    image = CloudinaryField('image', overwrite=True, resource_type='image') 
+    image = CloudinaryField('image', overwrite=True, resource_type='image')
+    slug = models.SlugField(max_length=100, unique=True, blank=False, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.description[:30])  # Generate slug from first 30 characters of description
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
