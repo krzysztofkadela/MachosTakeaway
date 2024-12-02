fe504f85dde2338bdec41e8c8e82482d3407f639
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-12-02 21:17:01 +0000

    Feat: Update form for make_reservation.

--- reservation/models.py
+++ reservation/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Reservation(models.Model):
-    customer_name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Link to the user model
     reservation_date = models.DateTimeField(auto_now_add=True)  # Automatically set when created
     number_of_guests = models.PositiveIntegerField() 
     contact_number = models.CharField(max_length=15)
@@ -10,4 +11,4 @@ class Reservation(models.Model):
     booking_time = models.TimeField()   # Field for the time of booking
 
     def __str__(self):
-        return f'Reservation for {self.customer_name} on {self.booking_date} at {self.booking_time}'
+        return f'Reservation for {self.user.username} on {self.booking_date} at {self.booking_time}'
--- reservation/views.py
+++ reservation/views.py
@@ -6,10 +6,12 @@ def make_reservation(request):
     if request.method == 'POST':
         form = ReservationForm(request.POST)
         if form.is_valid():
-            form.save()
+            reservation = form.save(commit=False)  # Do not save to DB yet
+            reservation.user = request.user  # Associate the logged-in user
+            reservation.save()  # Now save the reservation
             messages.success(request, 'Your reservation has been made successfully!')
-            return redirect('index')  # Redirect to an appropriate after-save page
+            return redirect('index')  # Redirect to an appropriate page after saving
     else:
         form = ReservationForm()
-    
+
     return render(request, 'reservation/make_reservation.html', {'form': form})
