18e920750282dc1c4531add8b4f4c0e8386fe1d5
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-12-03 21:33:41 +0000

    Feat: Update reservation settings models, add validationError for restaurant hours to ensure closing time is after opening time.

--- reservation/views.py
+++ reservation/views.py
@@ -5,6 +5,22 @@ from django.contrib.auth.decorators import login_required
 
 
 @login_required  # Ensures the user is logged in
+def make_reservationold(request):
+    if request.method == 'POST':
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            reservation = form.save(commit=False)  # Do not save to DB yet
+            reservation.user = request.user  # Associate the logged-in user
+            reservation.save()  # Now save the reservation
+            messages.success(request, 'Your reservation has been made successfully!')
+            return redirect('index')  # Redirect to an appropriate page after saving
+    else:
+        form = ReservationForm()
+
+    return render(request, 'reservation/make_reservation.html', {'form': form})
+
+
+@login_required  # Ensures the user is logged in
 def make_reservation(request):
     if request.method == 'POST':
         form = ReservationForm(request.POST)
