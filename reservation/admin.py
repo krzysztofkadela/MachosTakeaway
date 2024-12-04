30bea17c8c871beb263d9991d1bba1dabd7bb887
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-12-04 20:18:18 +0000

    Feat: Register reservation model in admin.py

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
-from .models import RestaurantSettings
+from .models import Reservation, RestaurantSettings
 
 # Register your models here.
 
-admin.site.register(RestaurantSettings)
+# Reservation model for Django admin
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'booking_date', 'booking_time', 'number_of_guests', 'is_approved')  # Fields to display in the admin list view
+    list_filter = ('booking_date', 'number_of_guests', 'is_approved')  # Filters to apply in the list view
+    search_fields = ('user__username', 'contact_number')  # Fields to provide search functionality
+
+# RestaurantSettings model for Django admin
+@admin.register(RestaurantSettings)
+class RestaurantSettingsAdmin(admin.ModelAdmin):
+    list_display = ('opening_time', 'closing_time')  # Fields to display in the admin list view
