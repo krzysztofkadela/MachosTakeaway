ed9e104bc243f545b241379a2dd5ac9a7362049c
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-12-14 23:53:39 +0000

    Style: Update add comment section style.

--- menu/models.py
+++ menu/models.py
@@ -17,11 +17,11 @@ class MenuItem(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
     image = CloudinaryField('image', overwrite=True, resource_type='image')
-    slug = models.SlugField(max_length=100, unique=True, blank=False, null=True)
+    slug = models.SlugField(max_length=100, unique=True, blank=True, null=True)
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.description[:30])  # Generate slug from first 30 characters of description
+            self.slug = slugify(self.description[:20])  # Generate slug from first 30 characters of description
         super().save(*args, **kwargs)
 
     def __str__(self):
