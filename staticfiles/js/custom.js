d3260a8267028c447dc42b91a891fd57a5b7b2d8
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-12-15 19:29:17 +0000

    Script: Move message display for 5s script in to custom.js file.

--- staticfiles/js/custom.js
+++ staticfiles/js/custom.js
@@ -61,3 +61,18 @@ $(".client_owl-carousel").owlCarousel({
 });
 
 
+//-- Display messages for 5 sec -->
+
+document.addEventListener('DOMContentLoaded', function () {
+    const alerts = document.querySelectorAll('.auto-close-alert');
+    alerts.forEach(alert => {
+        setTimeout(() => {
+            alert.classList.remove('show'); // Remove the 'show' class to hide the alert
+            alert.classList.add('fade'); // Add 'fade' class to smoothly fade it out
+            // Remove from DOM
+            setTimeout(() => {
+                alert.remove(); //  remove the alert from the DOM
+            }, 500); // for css transmition
+        }, 5000); // Time in milliseconds (5000 ms = 5 seconds)
+    });
+});
